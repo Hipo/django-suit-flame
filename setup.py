ad6b6e66dbe4ec11d8f499280a8c9635291ec5d0
Yigit Guler <yigit@hipo.biz>
2015-08-19 15:53:42 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-suit-flame',
+    version='0.1',
+    packages=['django-suit-flame',
+              ],
+    include_package_data=True,
+    license='License',  # example license
+    description='See active users & autosave changes at django admin.',
+    long_description=README,
+    author='hipo',
+    author_email='pypi@hipolabs.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
